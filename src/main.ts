3b16dd7df1fdec5b629e9cf8bd84055365e8a07d
baishusama <baishusama@outlook.com>
2018-08-28 08:23:30 +0800

    initialize gulpfile

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { sayHello } from './greet';
+
+function showHello(divId: string, name: string) {
+    const el = document.getElementById(divId);
+    el.innerText = sayHello(name);
+}
+
+showHello('greeting', 'TypeScript');
