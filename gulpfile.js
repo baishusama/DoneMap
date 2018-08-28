d1c0744aa18815f7be655871c9e0fa7c97f796a3
baishusama <baishusama@outlook.com>
2018-08-28 20:45:46 +0800

    finish gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var source = require('vinyl-source-stream');
 var tsify = require('tsify');
 var watchify = require('watchify');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 var paths = {
     pages: ['src/*.html']
 };
@@ -25,8 +28,16 @@ gulp.task('copy-html', function() {
 
 function bundle() {
     return watchedBrowserify
+        .transform('babelify', {
+            presets: ['es2015'],
+            extensions: ['.ts']
+        })
         .bundle()
         .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 }
 
